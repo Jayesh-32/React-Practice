fdf8ff4f3abebfb48bfcf737732d0364ae5054bc
Jayesh-32 <jayeshnakashe2002@gmail.com>
2024-02-09 02:30:47 +0530

    commited new component

--- vite_react_basic/src/App.jsx
+++ vite_react_basic/src/App.jsx
@@ -1,11 +1,12 @@
 
-import './App.css'
 
+import Test from './test'
 function App() {
 
   return (
     <>
      <h1>This is first vite react project with the help of chai aur code</h1>
+     <Test/>
     </>
   )
 }
