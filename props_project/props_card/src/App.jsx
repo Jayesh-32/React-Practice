e11346d7ecd835ade47a6bec35369c97f58448c9
Jayesh-32 <jayeshnakashe2002@gmail.com>
2024-02-11 01:23:42 +0530

    props practice project commited

---
+++ props_project/props_card/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from '../components/Card'
+import './App.css'
+
+function App() {
+  
+
+
+  return (
+    <>
+      <h1 className='bg-yellow-500 text-black p-5 rounded-xl mb-5'>hello Jayu</h1>
+      <Card username="Vivek" />
+      <Card username="Sameer"/>
+    </>
+  )
+}
+
+export default App
